7685839056069e2201f53f89c273a29fdabdfb6d
ek12mv2 <ekk12mv2@gmail.com>
2023-04-06 23:34:53 +0900

    2023.04.06 - 라우터만들기(진행중)

--- practice01/routes/user.ts
+++ practice01/routes/user.ts
@@ -1,5 +1,24 @@
 import * as express from 'express'
 import * as bcrypt from 'bcrypt'
 import { isLoggedIn } from './middleware'
+import User from '../models/user'
 
 const router = express.Router()
+
+router.get('/', isLoggedIn, (req, res) => {})
+router.get('/', async (req, res, next) => {
+  try {
+    const exUser = await User.findOne({
+      where: {
+        userId: req.body.userId,
+      },
+    })
+
+    if (exUser) {
+      return res.status(403).send('이미 사용중인 아이디입니다.')
+    }
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
+})
