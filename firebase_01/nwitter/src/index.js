dcbfa5c32304f8539f2b81d7296aee26c741be34
loveAlakazam <dmsrkd1216@gmail.com>
2023-04-10 21:49:25 +0900

    :closed_lock_with_key: chore: add secrets
    secret for firebaseConfig

--- firebase_01/nwitter/src/index.js
+++ firebase_01/nwitter/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from './firebase'
+console.log(firebase)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
